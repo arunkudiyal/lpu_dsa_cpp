ecee13d09eb2b2128ae1ff55e8e69a639fa68a43
arunkudiyal <arunkudiyal@icloud.com>
2023-08-02 19:57:31 +0530

    Introduction to string

--- main.cpp
+++ main.cpp
@@ -13,19 +13,19 @@ int main() {
 
     // data types in cpp
     // 1. int -> 32bit signed integer | 4 bytes
-    int x;                                              // No memory is assigned to the variable
-    x = 100;                                            // Memory allocation begins
+    // int x;                                              // No memory is assigned to the variable
+    // x = 100;                                            // Memory allocation begins
 
-    cout << x << endl;
+    // cout << x << endl;
     // sizeof() -> memory size of a passed data type
-    cout << sizeof(int) << endl;                        // 4 bytes
+    // cout << sizeof(int) << endl;                        // 4 bytes
 
     // 2. charachter - -128 to + 127
-    char ch = 'a';
-    cout << ch << endl;
-    char ch2 = 100;                                     // ASCII code of the char
-    cout << ch2 << endl;
-    cout << sizeof(char) << endl;                       // 1 byte
+    // char ch = 'a';
+    // cout << ch << endl;
+    // char ch2 = 100;                                     // ASCII code of the char
+    // cout << ch2 << endl;
+    // cout << sizeof(char) << endl;                       // 1 byte
 
     // 3. boolean -> True / False
     // bool m = 0;         // false
@@ -36,20 +36,20 @@ int main() {
     // cout << m << " " << n << " " << z << " " << v << " " << b << endl;
     // cout << sizeof(bool) << endl;                    // 1 byte
 
-    bool m = true;
-    bool n = false;
-    cout << m  << " " << n << endl;
+    // bool m = true;
+    // bool n = false;
+    // cout << m  << " " << n << endl;
 
     // 4. float -> non-real numbers -> decimal point
-    float myFloat = 3.145678937489392729473892;         // 6 precision value
-    cout << myFloat << endl;
-    cout << sizeof(myFloat) << endl;                    // 4 byte
+    // float myFloat = 3.145678937489392729473892;         // 6 precision value
+    // cout << myFloat << endl;
+    // cout << sizeof(myFloat) << endl;                    // 4 byte
 
     // 5. wide charachter (wchar_t) :- Charcter pool
-    wchar_t w = L'A';                                   // ASCII value
+    // wchar_t w = L'a';                                   // ASCII value
     // wchar_t p[] = L"Arun Kudiyal";
-    cout << w << endl;
-    cout << sizeof(w) << endl;
+    // cout << "ASCII code is " << w << endl;
+    // cout << sizeof(w) << endl;
 
 
     // DATATYPE MODIFIERS
@@ -60,11 +60,48 @@ int main() {
     // 5. double
 
     // create a integer which takes 2 bytes of space
-    short int num1 = 32767;                             // -32768 to +32767
-    cout << sizeof(num1) << endl;                       // 2 bytes
+    // short int num1 = 32767;                             // -32768 to +32767
+    // cout << sizeof(num1) << endl;                       // 2 bytes
 
-    unsigned short int num2 = 32768;                     // +65534
-    cout << sizeof(num2) <<  endl;
+    // unsigned short int num2 = 32768;                     // +65534
+    // cout << sizeof(num2) <<  endl;
+
+    // long int num3 = 467583;
+    // cout << sizeof(num3) << endl;                       // 8 bytes -> (doubled)
+
+    // double num4 = 738827284;
+    // cout << sizeof(num4) << endl;                       // 8 bytes
+
+    // // double num5 = 45.6726281719;
+    // cout << sizeof(num4) << endl;
+    // cout << num4 << endl;
+
+
+    // DERIVED DATA TYPE
+    // 1. string object | NOT A PDT
+    // string str;                                             // 24 bytes
+    // cin >> str;                                             // delimiter :- space, enter
+    // cout << str << endl;
+    // string str2 = "Hello World";
+    // cout << sizeof(string) << endl;
+    // cout << sizeof(str2) << endl;
+    // cout << str2 << endl;
+
+    // Q:- Why we say string to be not a PDT but an object?
+    string str;                                             // c++ treats string as an object
+    getline(cin, str);                                      // delimiter :- enter
+    cout << str << endl;
+
+    // Access methods from the string class
+    // 1. on run-time, you can expand the value of a string -> push_back(char)
+    // ->  push_back(char) :- appends a character to the string
+    str.push_back('W');
+    cout << str << endl;
+
+    // 2. on run-time, you can shrink the value of a string -> pop_back()
+    // -> pop_back() :- remove the last charachter from the string
+    str.pop_back();
+    cout << str <<  endl;
 
     return 0;
 }
