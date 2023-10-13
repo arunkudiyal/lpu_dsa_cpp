c80431bb585d3ea3ff7e1839a163ff3fce3164dc
arunkudiyal <arunkudiyal@icloud.com>
2023-10-13 20:50:57 +0530

    Binary Tree Implementation

---
+++ BinaryTree.cpp
@@ -0,0 +1,69 @@
+// Binary Tree -
+// A tree with max of two children.
+
+// 1. Root :- First Node of the BT.
+// 2. Children :- Two Children - Left & Right
+
+// Node of Tree -
+// 1. data :- type? - any primitive value
+// 2. children :- left & right
+
+#include<iostream>
+using namespace std;
+// Way - 1 - Using structures
+// struct TreeNode {
+//     int data;
+//     TreeNode* left; TreeNode* right;
+// };
+
+// Way - 2 - Using class
+class TreeNode {
+    public:
+        int data;
+        TreeNode* left; TreeNode* right;
+        TreeNode(int key) {
+            this->data = key;
+            this->left = this->right = NULL;
+        }
+};
+// Example0
+//          0
+// Example1
+//          5
+//      1       2
+// Example2
+//          5
+//      2          
+//  1
+// Example3
+//          5
+//              1         
+//                  2
+class BinaryTree {
+    public:
+        TreeNode* root;     // 3 things = root, root->left, root-> right;
+        BinaryTree() {
+            this->root = NULL;
+        } 
+        void inOrderTraversal(TreeNode* root) {
+            // inOrder traversal of any BST is in Ascending Order.
+            if(root != NULL) {
+                inOrderTraversal(root->left);           // L
+                cout << root->data << " ";              // D
+                inOrderTraversal(root->right);          // R
+            }
+        }
+};
+
+// Problem - BST - 
+// Sequence for insertion (root < key --> append to LEFT | root > key --> append to RIGHT)
+int main() {
+    // Building the Binary Tree
+    BinaryTree bt = BinaryTree();
+    bt.root = new TreeNode(5);
+    bt.root->right = new TreeNode(1);
+    bt.root->right->right = new TreeNode(2);
+    bt.inOrderTraversal(bt.root);           // 1 5 2
+    cout<< endl;
+    return 0;
+}
