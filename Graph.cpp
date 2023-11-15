2b220b94619d1f206d948b9aa24930ab4f822bbf
arunkudiyal <arunkudiyal@icloud.com>
2023-11-15 18:23:06 +0530

    Graph Traversal - Updated

--- Graph.cpp
+++ Graph.cpp
@@ -85,43 +85,6 @@ class Graph {
                 cout << endl;
             }
         }
-
-        // Discuss on MSTs (Minimal Spanning Tree) 
-        // void primMST(int graph[V][V]) {
-        //     int V = this->numVertices;
-        //     int parent[V];
-        //     int key[V];
-        //     bool mstSet[V];
-        //     for (int i = 0; i < V; i++)
-        //         key[i] = INT_MAX, mstSet[i] = false;
-        //     key[0] = 0;
-        //     parent[0] = -1;
-        //     for (int count = 0; count < V - 1; count++) {
-        //         int u = minKey(key, mstSet);
-        //         mstSet[u] = true;
-        //         for (int v = 0; v < V; v++)
-        //             if (graph[u][v] && mstSet[v] == false
-        //                 && graph[u][v] < key[v])
-        //                 parent[v] = u, key[v] = graph[u][v];
-        //     }
-        //     printMST(parent, graph);
-        // }
-
-        // int minKey(int key[], bool mstSet[]) {
-        //     int min = INT_MAX, min_index;
-        //     for (int v = 0; v < V; v++)
-        //         if (mstSet[v] == false && key[v] < min)
-        //             min = key[v], min_index = v;
-        //     return min_index;
-        // }
-        
-        // void printMST(int parent[], int graph[V][V]) {
-        //     int V = this->numVertices;
-        //     cout << "Edge \tWeight\n";
-        //     for (int i = 1; i < V; i++)
-        //         cout << parent[i] << " - " << i << " \t"
-        //             << graph[i][parent[i]] << " \n";
-        // }
 };
 
 int main() {
@@ -137,7 +100,6 @@ int main() {
     graph.addEdge(2, 5);
     graph.addEdge(6, 5);
     graph.addEdge(2, 3);
-    graph.addEdge(3, 5);
     graph.addEdge(3, 5);
     graph.addEdge(3, 4);
     graph.addEdge(5, 4);
